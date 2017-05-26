38154b11916bdec920832d5eaa234295195d1c1c
tmdmc <tmdmc@users.noreply.github.com>
2017-05-26 16:16:23 +0200

    first page click behaviour 2 + icons

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ var handleProviderClick = function(e) {
 		window.location.href = "2.html";
 	}
 };
+var handleUrlInputChange = function(e) {
+	console.log('changeee');
+	$('.eoos-provider.custom').find('.eoos-provider-go').addClass('highlighted');
+};
 
 $(function() {
 
 	$('#provider-list').on('click.eoos', '.eoos-provider', handleProviderClick);
+	$('#provider-connector-url').on('input.eoos', handleUrlInputChange);
 
 });
