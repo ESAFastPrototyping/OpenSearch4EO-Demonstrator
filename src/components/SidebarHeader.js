47022224990b82c816b672b9e82bbcd34583d19b
jbalhar <jakub@balhar.net>
2018-08-28 14:50:33 +0200

    Clean warnings.

--- src/components/SidebarHeader.js
+++ src/components/SidebarHeader.js
@@ -1,4 +1,3 @@
-//eslint-disable jsx-a11y/alt-text
 import React from 'react';
 
 const SidebarHeader = ({searchService, resetProvider, resetCollection, selectedCollection, providerInfo, collectionInfo}) => {
@@ -12,16 +11,16 @@ const SidebarHeader = ({searchService, resetProvider, resetCollection, selectedC
             {dd &&
             <div className="sidebar-block header" id="provider-header" onClick={resetCollection}>
                 <span className="sidebar-header-name">Provider</span>
-                <a className="sidebar-header-change" onClick={resetProvider}><i className="fa fa-pencil-square-o"></i></a>
-                <a className="sidebar-header-info" onClick={providerInfo}><i className="fa fa-info-circle" aria-hidden="true"></i></a>
+                <a className="sidebar-header-change" onClick={resetProvider}><i className="fa fa-pencil-square-o"></i> </a>
+                <a className="sidebar-header-info" onClick={providerInfo}><i className="fa fa-info-circle" aria-hidden="true"></i> </a>
                 <span className="sidebar-header-content-name">{shortName}</span>
             </div>
             }
             {selectedCollection.properties &&
             <div className="sidebar-block header" id="collection-header">
                 <span className="sidebar-header-name">Collection</span>
-                <a className="sidebar-header-change" onClick={resetCollection}><i className="fa fa-pencil-square-o"></i></a>
-                <a className="sidebar-header-info" onClick={collectionInfo}><i className="fa fa-info-circle" aria-hidden="true"></i></a>
+                <a className="sidebar-header-change" onClick={resetCollection}><i className="fa fa-pencil-square-o"></i> </a>
+                <a className="sidebar-header-info" onClick={collectionInfo}><i className="fa fa-info-circle" aria-hidden="true"></i> </a>
                 <div className="sidebar-header-content-name">{selectedCollection.properties.identifier}</div>
             </div>
             }
