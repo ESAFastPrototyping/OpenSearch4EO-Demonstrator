2472b079ebeb6fa84ea98e4ea9f0a013c14e7478
jbalhar <jakub@balhar.net>
2018-10-23 11:18:20 +0200

    Improve the look and feel to be coherent throughout the application.

--- src/components/FoundProducts.js
+++ src/components/FoundProducts.js
@@ -89,12 +89,29 @@ export default class FoundProducts extends Component {
             resultsCount = this.props.productsResult.properties.totalResults;
         }
 
-        let info = (<Loader/>);
-        let infoStyle = {
-            marginTop: '30px'
+        const theme = {
+            scheme: 'bright',
+            author: 'chris kempson (http://chriskempson.com)',
+            base00: '#000000',
+            base01: '#303030',
+            base02: '#505050',
+            base03: '#b0b0b0',
+            base04: '#d0d0d0',
+            base05: '#e0e0e0',
+            base06: '#f5f5f5',
+            base07: '#ffffff',
+            base08: '#fb0120',
+            base09: '#fc6d24',
+            base0A: '#fda331',
+            base0B: '#a1c659',
+            base0C: '#76c7b7',
+            base0D: '#6fb3d2',
+            base0E: '#d381c3',
+            base0F: '#be643c'
         };
+        let info = (<Loader/>);
         if(this.state.info) {
-            info = (<JSONTree data={this.state.info}/>)
+            info = (<JSONTree data={this.state.info} theme={theme}/>)
         }
 
         return (
@@ -113,9 +130,7 @@ export default class FoundProducts extends Component {
                 </div>
                 {(this.state.fetchingInfo || this.state.info) &&
                 <div id="info-panel">
-                    <div style={infoStyle}>
-                        {info}
-                    </div>
+                    {info}
                     <div id="close-info-panel" className="fa fa-window-close" onClick={() => this.closeInfo()}></div>
                 </div>
                 }
